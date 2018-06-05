c064e81d7c3ffd4e3149d1de884fec340ac5b883
Pleuni Pennings <pleunipennings@sfs-wifi-dhcp-10-143-130-104.wifi.sfsu.edu>
2018-06-05 10:53:29 -0700

    Updated list of resistance mutations according to https://hivdb.stanford.edu/assets/media/resistance-mutation-handout-Dec2017.b8f72e32.pdf

--- Rscripts/ResistanceMutationsHIVDB.r
+++ Rscripts/ResistanceMutationsHIVDB.r
@@ -1,18 +1,18 @@
 #Major HIV-1 Drug Resistance Mutations 
 #Updated March 9, 2015 
-#Updated summary from the HIV Drug Resistance Database. This document can be downloaded from the http://hivdb.stanford.edu home page. Detailed and referenced versions of each drug class summary can be found at http://hivdb.stanford.edu/pages/drugSummaries.html 
+#Updated June 2018 https://hivdb.stanford.edu/assets/media/resistance-mutation-handout-Dec2017.b8f72e32.pdf
 
 
-NNRTImuts<-data.frame("pos"=numeric(10),"wt"=numeric(10),"mut"=numeric(10))
-NNRTImuts$pos<-c(100,101,103,106,138,179,181,188,190,227)
-NNRTImuts$mut<-c("I","PEH","NS","MA","KAGQ","DEF","CIV","LCH","SAEQ","LC")
+NNRTImuts<-data.frame("pos"=numeric(8),"wt"=numeric(8),"mut"=numeric(8))
+NNRTImuts$pos<-c(100,101,103,106,181,188,190,230)
+NNRTImuts$mut<-c("I","PE","NS","MA","CIV","LCH","SAEQ","L")
 NRTImuts<-data.frame("pos"=numeric(11),"wt"=numeric(11),"mut"=numeric(11))
 NRTImuts$pos<-c(41,65,67,70,74,115,151,184,210,215,219)
-NRTImuts$mut<-c("L","R","N","ER","IV","F","M","IV","W","YF","QE")
-PImuts<-data.frame("pos"=numeric(12),"wt"=numeric(12),"mut"=numeric(12))
-PImuts$pos<-c(24,32,46,47,48,50,54,76,82,84,88,90)
-PImuts$mut<-c("I","I","IL","VA","VM","LV","VTALM","V","AFTS","V","S","M")
-NumImportantMuts=10+11+12
+NRTImuts$mut<-c("L","R","N","E","IV","F","M","IV","W","YF","QE")
+PImuts<-data.frame("pos"=numeric(11),"wt"=numeric(11),"mut"=numeric(11))
+PImuts$pos<-c(32,46,47,48,50,54,76,82,84,88,90)
+PImuts$mut<-c("I","IL","VA","VM","LV","VTALM","V","AFTS","V","S","M")
+NumImportantMuts=length(c(NNRTImuts,NRTImuts,PImuts))
 PositionsRT<-sort(c(NNRTImuts$pos,NRTImuts$pos))
 PositionsPRO<-PImuts$pos
 
